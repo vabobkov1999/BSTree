b4e29889357802733a388d6ede8d4ac0fc3bd4d7
Vasily <34238099+MorozzoFFF@users.noreply.github.com>
2018-05-29 20:45:14 +0300

    Update example2.cpp

--- examples/example2.cpp
+++ examples/example2.cpp
@@ -11,17 +11,12 @@ auto change_color(int c) -> void;
 int main(int argc, char* argv[]) {
     Tree tree;
     int n = 0;
-
-    for (unsigned int i = 1; i < argc; ++i) ++n;
-
-    int* MassTree_curr = new int[n];
-
-    for (unsigned i = 0; i < n; ++i) MassTree_curr[i] = atoi(argv[i + 1]);
-
-    int* MassTree = new int[n];
-
-    for (unsigned i = 0; i < n; ++i)
-        for (unsigned j = i + 1; j < n; ++j) {
+      for (unsigned int i = 1; i < argc; ++i) ++n;
+        int* MassTree_curr = new int[n];
+          for (unsigned i = 0; i < n; ++i) MassTree_curr[i] = atoi(argv[i + 1]);
+            int* MassTree = new int[n];
+              for (unsigned i = 0; i < n; ++i)
+                for (unsigned j = i + 1; j < n; ++j) {
             if (MassTree_curr[i] == MassTree_curr[j]) MassTree_curr[j] = 0;
         }
     int k = 0;
@@ -51,15 +46,25 @@ int main(int argc, char* argv[]) {
 auto menu(Tree & tree) -> void {
     while (1) {
         change_color(CYAN);
+        
         cout << "Выберите одну из операций: " << endl;
+        
         cout << "1. Вывести дерево на экран" << endl;
+        
         cout << "2. Вывести список узлов дерева" << endl;
+        
         cout << "3. Добавить узел в дерево" << endl;
+        
         cout << "4. Удалить узел из дерева" << endl;
+        
         cout << "5. Сохранить дерево в файл" << endl;
+        
         cout << "6. Загрузить дерево из файла" << endl;
+        
         cout << "7. Проверить наличие узла" << endl;
+        
         cout << "8. Завершить работу программы" << endl;
+        
         change_color(GREEN);
         int choise = 0;
         cin >> choise;
@@ -70,6 +75,7 @@ auto menu(Tree & tree) -> void {
             case 1:
                 tree.print_tree();
                 break;
+                    
             case 2: {
                 change_color(GREEN);
                 cout << "Ввести списков узлов дерева:" << endl;
@@ -91,6 +97,7 @@ auto menu(Tree & tree) -> void {
                 cout << endl;
             }
             break;
+                    
             case 3: {
                 change_color(GREEN);
                 cout << "Введите значение добавляемого элемента: ";
@@ -106,6 +113,7 @@ auto menu(Tree & tree) -> void {
                 }
             }
             break;
+                    
             case 4: {
                 change_color(GREEN);
                 cout << "Введите значение удаляемого элемента: ";
@@ -121,9 +129,10 @@ auto menu(Tree & tree) -> void {
                 }
             }
             break;
+                    
             case 8: {
                 change_color(RED);
-                cout << "Вы уверены, что хотите выйти?(yes|no)" << endl;
+                cout << "Вы точно хотите выйти?(yes|no)" << endl;
                 change_color(GREEN);
                 string prog_exit;
                 cin >> prog_exit;
